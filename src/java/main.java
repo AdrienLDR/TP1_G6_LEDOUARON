d825590ea26ac96b005310fa84083078908de27c
Adrien <ledouaronadrien@gmail.com>
2023-02-19 23:58:48 +0100

    TP2 discriminant

--- src/java/main.java
+++ src/java/main.java
@@ -1,26 +1,31 @@
 import java.util.Scanner;
 
 public class main {
-
-    public static void volume() {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Entrez la longueur du pavé droit :");
-        float longueur = scanner.nextFloat();
-
-        System.out.println("Entrez la largeur du pavé droit :");
-        float largeur = scanner.nextFloat();
-
-        System.out.println("Entrez la hauteur du pavé droit :");
-        float hauteur = scanner.nextFloat();
-
-        float volume = longueur * largeur * hauteur;
-
-        System.out.println("Le volume du pavé droit est de " + volume + " m³");
-    }
-
     public static void main(String[] args) {
-        volume();
+        discriminant();
     }
+    public static void discriminant() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Quelle est la valeur de a ?");
+        int a = scanner.nextInt();
+        System.out.println("Quelle est la valeur de b ?");
+        int b = scanner.nextInt();
+        System.out.println("Quelle est la valeur de c ?");
+        int c = scanner.nextInt();
+        int delta = (int) (Math.pow(b, 2) - 4 * a * c);
+        if (delta < 0) {
+            System.out.println("Ce polynome n’a pas de racine reelle");
+        } else if (delta == 0) {
+            double x0 = (double) -b / (2 * a);
+            System.out.println("Le polynome a une racine double : x0 = " + x0);
+        } else {
+            double x0 = (-b + Math.sqrt(delta)) / (2 * a);
+            double x1 = (-b - Math.sqrt(delta)) / (2 * a);
+            System.out.println("Le polynome a deux racines distinctes : x0 = " + x0 + " et x1 = " + x1);
+        }
+    }
+
+
+
 }
 
