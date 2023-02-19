f76523d2d8bd1201db255df7607cb87c537e2a6a
Adrien <ledouaronadrien@gmail.com>
2023-02-19 23:54:22 +0100

    TP Pave droit

--- src/java/main.java
+++ src/java/main.java
@@ -1,24 +1,26 @@
-
 import java.util.Scanner;
 
 public class main {
-    public static void main(String[] args) {
-        division();
+
+    public static void volume() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Entrez la longueur du pavé droit :");
+        float longueur = scanner.nextFloat();
+
+        System.out.println("Entrez la largeur du pavé droit :");
+        float largeur = scanner.nextFloat();
+
+        System.out.println("Entrez la hauteur du pavé droit :");
+        float hauteur = scanner.nextFloat();
+
+        float volume = longueur * largeur * hauteur;
+
+        System.out.println("Le volume du pavé droit est de " + volume + " m³");
     }
 
-
-    public static void division() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Veuillez saisir le premier entier : ");
-        int a = scanner.nextInt();
-        System.out.println("Veuillez saisir le deuxième entier : ");
-        int b = scanner.nextInt();
-        if (b == 0) {
-            System.out.println("Division par zéro impossible !");
-        } else {
-            float resultat = (float) a / (float) b;
-            System.out.println("Le résultat de " + a + " / " + b + " est : " + resultat);
-        }
+    public static void main(String[] args) {
+        volume();
     }
 }
 
