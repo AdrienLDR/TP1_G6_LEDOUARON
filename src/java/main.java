0553dff1c6e6aab6254e2db6e5df00440310d570
Adrien <ledouaronadrien@gmail.com>
2023-02-19 23:47:23 +0100

    TP division

--- src/java/main.java
+++ src/java/main.java
@@ -3,17 +3,22 @@ import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
-        somme();
+        division();
     }
 
-    public static void somme() {
+
+    public static void division() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Veuillez saisir le premier entier");
-        int premierEntier = scanner.nextInt();
-        System.out.println("Veuillez saisir le deuxième entier");
-        int deuxiemeEntier = scanner.nextInt();
-        int somme = premierEntier + deuxiemeEntier;
-        System.out.println("La somme de " + premierEntier + " avec " + deuxiemeEntier + " est égale à " + somme);
+        System.out.println("Veuillez saisir le premier entier : ");
+        int a = scanner.nextInt();
+        System.out.println("Veuillez saisir le deuxième entier : ");
+        int b = scanner.nextInt();
+        if (b == 0) {
+            System.out.println("Division par zéro impossible !");
+        } else {
+            float resultat = (float) a / (float) b;
+            System.out.println("Le résultat de " + a + " / " + b + " est : " + resultat);
+        }
     }
 }
 
