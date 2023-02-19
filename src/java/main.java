bd4d5dfbf2a8cf517b491604218b6c47dd573264
Adrien <ledouaronadrien@gmail.com>
2023-02-20 00:03:13 +0100

    TP2 Parité

--- src/java/main.java
+++ src/java/main.java
@@ -2,30 +2,19 @@ import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
-        discriminant();
+        parite();
     }
-    public static void discriminant() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Quelle est la valeur de a ?");
-        int a = scanner.nextInt();
-        System.out.println("Quelle est la valeur de b ?");
-        int b = scanner.nextInt();
-        System.out.println("Quelle est la valeur de c ?");
-        int c = scanner.nextInt();
-        int delta = (int) (Math.pow(b, 2) - 4 * a * c);
-        if (delta < 0) {
-            System.out.println("Ce polynome n’a pas de racine reelle");
-        } else if (delta == 0) {
-            double x0 = (double) -b / (2 * a);
-            System.out.println("Le polynome a une racine double : x0 = " + x0);
-        } else {
-            double x0 = (-b + Math.sqrt(delta)) / (2 * a);
-            double x1 = (-b - Math.sqrt(delta)) / (2 * a);
-            System.out.println("Le polynome a deux racines distinctes : x0 = " + x0 + " et x1 = " + x1);
+        public static void parite() {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Veuillez saisir un entier :");
+            int nombre = scanner.nextInt();
+
+            if (nombre % 2 == 0) {
+                System.out.println(nombre + " est pair.");
+            } else {
+                System.out.println(nombre + " est impair.");
+            }
         }
-    }
-
-
 
 }
 
